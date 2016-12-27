131116c18997a0314e89fb221ac89f901e88c5fc
Lucas Winningham <lucas.winningham@gmail.com>
2016-12-27 13:43:23 +0000

    Added user profile styles

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,7 +1,3 @@
 $(document).on('turbolinks:load', function(){
-  // $('.alert').delay(125).fadeOut(100);
-  // $('.alert').delay(125).fadeIn(100);
-  // $('.alert').delay(125).fadeOut(100);
-  // $('.alert').delay(125).fadeIn(100);
   $('.alert').delay(2500).fadeOut(5000);
 });
