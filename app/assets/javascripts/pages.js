e7dc66600db6bcfc6150cd4920092977f10f5294
Lucas Winningham <lucas.winningham@gmail.com>
2016-12-27 05:42:54 +0000

    Added bare bones user show page and updated profiles redirect

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,7 @@
 $(document).on('turbolinks:load', function(){
-  $('.alert').delay(3000).fadeOut(1000);
+  // $('.alert').delay(125).fadeOut(100);
+  // $('.alert').delay(125).fadeIn(100);
+  // $('.alert').delay(125).fadeOut(100);
+  // $('.alert').delay(125).fadeIn(100);
+  $('.alert').delay(2500).fadeOut(5000);
 });
