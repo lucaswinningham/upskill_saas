0cc660fadd7d491818cc7f4bb3382c35bc8b13d6
Lucas Winningham <lucas.winningham@gmail.com>
2016-12-27 03:33:06 +0000

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,4 +3,27 @@ class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
   end
+  
+  # POST request to /users/:user_id/profile
+  def create
+    # Get correct user
+    @user = User.find(params[:user_id])
+    
+    # Create profile linked to this specific user
+    @profile = @user.build_profile(profile_params)
+    
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      render action :new
+    end
+  end
+  
+  private
+    # To collect data from form, use strong parameters
+    # Whitelist the form fields
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
