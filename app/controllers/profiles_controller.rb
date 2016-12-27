dc17ed73bcd2f2cba6b0d120cf40d7c5b754794a
Lucas Winningham <lucas.winningham@gmail.com>
2016-12-27 13:24:34 +0000

    Added file field for image upload to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -24,6 +24,13 @@ class ProfilesController < ApplicationController
     # To collect data from form, use strong parameters
     # Whitelist the form fields
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name,
+                                      :last_name,
+                                      :avatar,
+                                      :job_title,
+                                      :phone_number,
+                                      :contact_email,
+                                      :description
+      )
     end
 end
